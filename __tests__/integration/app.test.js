3f1fbb92cb357f646780a7d30add26826bd92847
Agata-M-S <agata.webdevelopment@gmail.com>
2023-11-20 16:43:35 +0000

    submit task 3 GET/api provides a description of all avaliable endpoints

--- __tests__/integration/app.test.js
+++ __tests__/integration/app.test.js
@@ -20,45 +20,76 @@ describe("GET /api/servercheck", () => {
 			});
 	});
 });
-
-describe("GET /api/topics", () => {
-	test("200: responds with an array of topic objects", () => {
-		return request(app)
-			.get("/api/topics")
-			.expect(200)
-			.then(({ body }) => {
-				const { topics } = body;
-
-				expect(topics).toBeInstanceOf(Array);
-				expect(topics).toHaveLength(3);
-			});
-	});
-	test("each object should have properties: slug, description", () => {
-		return request(app)
-			.get("/api/topics")
-			.expect(200)
-			.then(({ body }) => {
-				const { topics } = body;
-
-				expect(topics).toBeInstanceOf(Array);
-				expect(topics).toHaveLength(3);
-
-				topics.forEach((topic) => {
-					expect(topic).toMatchObject({
-						slug: expect.any(String),
-						description: expect.any(String),
-					});
-				});
-			});
-	});
-	describe("Error handling", () => {
-		test("400: /api/notavalidpath", () => {
+describe("GET methods", () => {
+	describe("GET /api/topics", () => {
+		test("200: responds with an array of topic objects", () => {
 			return request(app)
-				.get("/api/notavalidpath")
-				.expect(404)
+				.get("/api/topics")
+				.expect(200)
 				.then(({ body }) => {
-					expect(body.msg).toBe("path does not exist");
+					const { topics } = body;
+
+					expect(topics).toBeInstanceOf(Array);
+					expect(topics).toHaveLength(3);
 				});
 		});
+		test("each object should have properties: slug, description", () => {
+			return request(app)
+				.get("/api/topics")
+				.expect(200)
+				.then(({ body }) => {
+					const { topics } = body;
+
+					expect(topics).toBeInstanceOf(Array);
+					expect(topics).toHaveLength(3);
+
+					topics.forEach((topic) => {
+						expect(topic).toMatchObject({
+							slug: expect.any(String),
+							description: expect.any(String),
+						});
+					});
+				});
+		});
+	});
+	describe("GET /api returns an object with a description of all other avaliable endpoints ", () => {
+		test("200: responds with a JSON object", () => {
+			return request(app)
+				.get("/api")
+				.expect(200)
+				.then(({ body }) => {
+					const { endpoints } = body;
+					expect(endpoints).toBeInstanceOf(Object);
+					expect(endpoints).not.toBeInstanceOf(Array);
+				});
+		});
+		test("each object has properies: description, queries, exampleResponse", () => {
+			return request(app)
+				.get("/api")
+				.expect(200)
+				.then(({ body }) => {
+					const { endpoints } = body;
+					console.log(endpoints, "{<<<<<");
+					for (const [key, value] of Object.entries(endpoints)) {
+            console.log(value,'<<<endpoint')
+						expect(value).toMatchObject({
+							description: expect.any(String),
+							queries: expect.any(Array),
+							exampleResponse: expect.any(Object),
+						});
+					}
+				});
+		});
+	});
+});
+
+describe("Error handling GET", () => {
+	test("400: /api/notavalidpath", () => {
+		return request(app)
+			.get("/api/notavalidpath")
+			.expect(404)
+			.then(({ body }) => {
+				expect(body.msg).toBe("path does not exist");
+			});
 	});
 });
--- models/topics.models.js
+++ models/topics.models.js
@@ -1,4 +1,5 @@
 const db = require("../db/connection");
+const fs = require("fs/promises");
 
 exports.selectTopics = () => {
 	let queryStr = `SELECT * FROM topics`;
@@ -6,4 +7,12 @@ exports.selectTopics = () => {
 	return db.query(queryStr).then(({ rows }) => {
 		return rows;
 	});
+};
+
+exports.selectAllEndpoints = () => {
+	return fs
+		.readFile(`${__dirname}/../endpoints.json`)
+		.then((endpoints) => {
+			return JSON.parse(endpoints);
+		})
 };
