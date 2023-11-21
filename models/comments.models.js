e503d9d23d56a1dbaebacf3dba7b0d6837f66960
Agata-M-S <agata.webdevelopment@gmail.com>
2023-11-21 17:00:17 +0000

    complete task 6 -- GET/api/articles/:article_id/comments

---
+++ models/comments.models.js
@@ -0,0 +1,12 @@
+const db = require("../db/connection");
+
+
+exports.selectCommentsByArticleId = (article_id) => {
+	const sortBy = ` ORDER BY created_at DESC`;
+
+	let queryStr = `SELECT * FROM comments WHERE article_id = $1 ${sortBy} `;
+
+	return db.query(queryStr, [article_id]).then(({ rows }) => {
+		return rows;
+	});
+};
