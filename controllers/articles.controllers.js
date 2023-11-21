0fabd11be7e46537e70916de0fd49cc669ff4df7
Agata-M-S <agata.webdevelopment@gmail.com>
2023-11-21 14:41:50 +0000

    complete task 5 -- add /api/GET/articles endpoint

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,11 +1,18 @@
-const { selectArticle } = require("../models/articles.models");
+const { selectArticleById, selectAllArticles } = require("../models/articles.models");
 
 exports.getArticleById = (req, res, next) => {
 	const { article_id } = req.params;
 
-	selectArticle(article_id)
+	selectArticleById(article_id)
 		.then((article) => {
 			res.status(200).send({ article });
 		})
 		.catch(next);
+};
+
+exports.getAllArticles = (req, res, next) => {
+	selectAllArticles()
+  .then((articles)=>{
+    res.status(200).send({articles})
+  });
 };
