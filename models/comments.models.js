5303cb1f9d13f4e2f67e22923b8b4cbde0ab8228
Agata-M-S <agata.webdevelopment@gmail.com>
2023-11-23 10:07:54 +0000

    complete task 9 --add: DELETE/api/comments/:comment_id

--- models/comments.models.js
+++ models/comments.models.js
@@ -27,3 +27,30 @@ exports.insertCommentsByArticleId = (article_id, insert) => {
 			return rows[0];
 		});
 };
+
+exports.selectCommentById = (comment_id) => {
+	return db
+		.query(
+			`SELECT * FROM comments 
+      WHERE comment_id = $1`,
+			[comment_id]
+		)
+		.then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({ status: 404, msg: "comment does not exist" });
+      }
+			return rows[0];
+		});
+};
+
+exports.removeCommentById = (comment_id) => {
+	return db
+		.query(
+			`DELETE FROM comments
+      WHERE comment_id = $1`,
+			[comment_id]
+		)
+		.then(() => {
+			console.log("comment was sucessfully removed");
+		});
+};
