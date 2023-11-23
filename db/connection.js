ea999a56d1200bc39ba39d6e81784359736b7181
Agata-M-S <agata.webdevelopment@gmail.com>
2023-11-23 14:57:24 +0000

    add config var

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const ENV = process.env.NODE_ENV || "development";
 require("dotenv").config({
 	path: `${__dirname}/../.env.${ENV}`,
 });
-
+const config = {}
 if (ENV === "production") {
 	config.connectionString = process.env.DATABASE_URL;
 	config.max = 2;
